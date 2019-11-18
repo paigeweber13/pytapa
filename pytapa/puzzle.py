ee1abcb712088ef474c0ceeca96c6b33a9f8907f
Noal Smith <noalssmith2@live.com>
2019-11-17 18:29:15 -0700

    added board class

--- pytapa/puzzle.py
+++ pytapa/puzzle.py
@@ -1,6 +1,9 @@
 class Board():
-    pass
-
+    puzzleSolved = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+    puzzleDefault = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+    puzzle = puzzleDefault
+    rectangles = []
+    solved = False
 
 class Hint():
     pass
