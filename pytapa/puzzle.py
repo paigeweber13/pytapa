827ca308c53a061b861a44e533d14aacbb8c263b
Noal Smith <noalssmith2@live.com>
2019-11-17 18:47:37 -0700

    added checkSolved function

--- pytapa/puzzle.py
+++ pytapa/puzzle.py
@@ -1,9 +1,15 @@
 class Board():
-    puzzleSolved = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+    #puzzleSolved = [[1, 1, 1], [1, 0, 1], [1, 1, 1]]
+    puzzleSolved = [[1, 0, 0], [0, 0, 0], [0, 0, 0]]
     puzzleDefault = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
     puzzle = puzzleDefault
     rectangles = []
     solved = False
+
+    def checkSolved(self):
+        if self.puzzle == self.puzzleSolved:
+            self.solved = True
+
 
 class Hint():
     pass
