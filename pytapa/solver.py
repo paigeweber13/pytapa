846a57318425d4e2b8ae9faf142abe56df7d367a
Noal Smith <noalssmith2@live.com>
2019-11-20 13:30:48 -0700

    made the solver a bit more programatic

--- pytapa/solver.py
+++ pytapa/solver.py
@@ -11,16 +11,20 @@ def solve(puzzle):
                 if puzzle[row][col] >= 3:
                     if puzzle[row][col] == 8:
                         if 0 < row < len(puzzle)-1 and 0 < col < len(puzzle)-1:
-                            puzzle[row-1][col-1] = 1
-                            puzzle[row-1][col] = 1
-                            puzzle[row-1][col+1] = 1
-                            puzzle[row][col-1] = 1
-                            puzzle[row][col+1] = 1
-                            puzzle[row+1][col-1] =1
-                            puzzle[row+1][col] = 1
-                            puzzle[row+1][col+1] = 1
+                            vr = -2
+                            for i in range(len(puzzle)):
+                                vr += 1
+                                hz = -2
+                                for j in range(len(puzzle[row])):
+                                    hz += 1
+                                    if puzzle[row+vr][col+hz] >= 3:
+                                        continue
+                                    else:
+                                        puzzle[row+vr][col+hz] = 1
+                                    
+                                
         print(puzzle)
         break
 
 default = [[0,0,0],[0,8,0],[0,0,0]]
-solve(default)
+solution = solve(default)
