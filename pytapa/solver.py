d6605d39db88590a5b9866e9ec5d9685cdbbfcaa
Noal Smith <noalssmith2@live.com>
2019-11-20 16:17:28 -0700

    solve for hint4

--- pytapa/solver.py
+++ pytapa/solver.py
@@ -31,6 +31,70 @@ def checkHint(coordinate, hint, x, y):
         ls = len(ls)
         return ls
 
+def checkHintTop(coordinate, hint, x, y):
+    if coordinate[x][y] == hint:
+        ls = []
+        vr = -1
+        for i in range(2):
+            vr += 1
+            hz = -2
+            for j in range(3):
+                hz += 1
+                if coordinate[x+vr][y+hz] >= 2:
+                    continue
+                else:
+                    ls.append(coordinate[x+vr][y+hz])
+        ls = len(ls)
+        return ls
+
+def checkHintBottom(coordinate, hint, x, y):
+    if coordinate[x][y] == hint:
+        ls = []
+        vr = -2
+        for i in range(2):
+            vr += 1
+            hz = -2
+            for j in range(3):
+                hz += 1
+                if coordinate[x+vr][y+hz] >= 2:
+                    continue
+                else:
+                    ls.append(coordinate[x+vr][y+hz])
+        ls = len(ls)
+        return ls
+
+def checkHintSideL(coordinate, hint, x, y):
+    if coordinate[x][y] == hint:
+        ls = []
+        vr = -2
+        for i in range(3):
+            vr += 1
+            hz = -1
+            for j in range(2):
+                hz += 1
+                if coordinate[x+vr][y+hz] >= 2:
+                    continue
+                else:
+                    ls.append(coordinate[x+vr][y+hz])
+        ls = len(ls)
+        return ls
+
+def checkHintSideR(coordinate, hint, x, y):
+    if coordinate[x][y] == hint:
+        ls = []
+        vr = -2
+        for i in range(3):
+            vr += 1
+            hz = -2
+            for j in range(2):
+                hz += 1
+                if coordinate[x+vr][y+hz] >= 2:
+                    continue
+                else:
+                    ls.append(coordinate[x+vr][y+hz])
+        ls = len(ls)
+        return ls          
+
 def checkPosition(coordinate, x, y):
     if x == 0:
         position = 'top'
@@ -43,7 +107,6 @@ def checkPosition(coordinate, x, y):
     else:
         position = 'center'
     return position
-
 
 def solveHintTop(coordinate, hint, x, y):
     if coordinate[x][y] == hint:
@@ -105,15 +168,18 @@ def solve(default):
             for col in range(len(default[row])):
                 if default[row][col] >= 3:
                     # Solves hint8 
-                    solveHint(default, 8, row, col)
+                    if default[row][col] == 8:
+                        solveHint(default, 8, row, col)
                     # Solves hint7  
-                    ls7 = checkHint(default, 7, row, col)
-                    if ls7 == 7:
-                        solveHint(default, 7, row, col)
+                    if default[row][col] == 7:
+                        ls7 = checkHint(default, 7, row, col)
+                        if ls7 == 7:
+                            solveHint(default, 7, row, col)
                     # Solves hint6
-                    ls6 = checkHint(default, 6, row, col)
-                    if ls6 == 6:
-                        solveHint(default, 6, row, col)
+                    if default[row][col] == 6:
+                        ls6 = checkHint(default, 6, row, col)
+                        if ls6 == 6:
+                            solveHint(default, 6, row, col)
                     # Solves hint5
                     if default[row][col] == 5: 
                         pos = checkPosition(default, row, col)
@@ -130,10 +196,53 @@ def solve(default):
                             if ls5 == 5:
                                 solveHint(default, 5, row, col)
                     # Solves hint4
-                    
+                    if default[row][col] == 4:
+                        pos = checkPosition(default, row, col)
+                        if pos == 'top':
+                            ls4 = checkHintTop(default, 4, row, col)
+                            if ls4 == 4:
+                                solveHintTop(default, 4, row, col)
+                            else:
+                                num = -1
+                                for i in range(3):
+                                    default[row+1][col+num] = 1
+                                    num += 1
+                        elif pos == 'bottom':
+                            ls4 = checkHintBottom(default, 4, row, col)
+                            if ls4 == 4:
+                                solveHintBottom(default, 4, row, col)
+                            else:
+                                num = -1
+                                for i in range(3):
+                                    default[row-1][col+num] = 1
+                                    num += 1
+                        elif pos == 'sideL':
+                            ls4 = checkHintSideL(default, 4, row, col)
+                            if ls4 == 4:
+                                solveHintSideL(default, 4, row, col)
+                            else:
+                                num = -1
+                                for i in range(3):
+                                    default[row+num][col+1] = 1
+                                    num += 1
+                        elif pos == 'sideR':
+                            ls4 = checkHintSideR(default, 4, row, col)
+                            if ls4 == 4:
+                                solveHintSideR(default, 4, row, col)
+                            else:
+                                num = -1
+                                for i in range(3):
+                                    default[row+num][col-1] = 1
+                                    num += 1
+                        elif pos == 'center':
+                            ls4 = checkHint(default, 4, row, col)
+                            if ls4 == 4:
+                                solveHint(default, 4, row, col)
+                    # Solves hint3
+
                                                    
         print(default)
         break
 
-puzzledefault = [[0,0,0],[0,5,0],[0,0,0]]
+puzzledefault = [[0,0,0],[5,0,0],[0,0,0]]
 solution = solve(puzzledefault)
