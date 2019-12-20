42f546eb712d636882e984deb42057acc2ee6947
Riley <rileyw13@protonmail.com>
2019-12-20 17:50:57 -0500

    started basic validator

--- pytapa_env/solver.py
+++ pytapa_env/solver.py
@@ -1,13 +1,26 @@
-import puzzle
+from . import puzzle
 
 def validate_solution(solution):
     # TODO: implement validation
-    #   * is solution a square matrix?
-    #   * is every cell a valid code? (see puzzle.cell_codes)
     #   * does it form one continuous line?
     #   * are there no blocks of 2x2 or bigger?
     #   * does it obey hints?
-    pass
+    n = len(solution)
+
+    for row_i in range(len(solution)):
+        # is it square? are all rows of same length?
+        if len(solution[row_i]) != n:
+            print('Matrix not square! Row', row_i, ' is of length', 
+                  len(solution[row_i]), ' but matrix has height', n)
+            return False
+        
+        for col_i in range(len(solution[row_i])):
+            # is every cell a valid code?
+            if solution[row_i][col_i] not in puzzle.cell_codes:
+                print('Invalid entry:', solution[row_i][col_i])
+                return False
+    
+    return True
 
 def solve(puzzle):
     pass
