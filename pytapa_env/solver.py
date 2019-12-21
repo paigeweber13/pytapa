331ce6c05dc0de6b9ae4c0b2457401ee3450ae71
Riley <rileyw13@protonmail.com>
2019-12-21 17:15:32 -0500

    now checks for 2x2 blocks

--- pytapa_env/solver.py
+++ pytapa_env/solver.py
@@ -1,4 +1,24 @@
 from . import puzzle
+from . import solver
+
+def find_all(l: list, sub: list) -> list:
+    """
+    returns list of indices of all finds
+    """
+    res = []
+
+    for l_i in range(len(l)):
+        match = True
+
+        for sub_i in range(len(sub)):
+            if l_i+sub_i >= len(l) or l[l_i+sub_i] != sub[sub_i]:
+                match = False
+                break
+
+        if match:
+            res.append(l_i)
+    
+    return res
 
 def validate_solution(solution):
     # TODO: implement validation
@@ -14,12 +34,23 @@ def validate_solution(solution):
             print('Matrix not square! Row', row_i, ' is of length', 
                   len(solution[row_i]), ' but matrix has height', n)
             return False
+
+        # search for 2x2 blocks
+        if row_i + 1 != len(solution):
+            locations_this_row = solver.find_all(solution[row_i], [1,1])
+            locations_next_row = solver.find_all(solution[row_i+1], [1,1])
+            for loc_this_row in locations_this_row:
+                for loc_next_row in locations_next_row:
+                    if loc_this_row == loc_next_row:
+                        return False
         
         for col_i in range(len(solution[row_i])):
             # is every cell a valid code?
             if solution[row_i][col_i] not in puzzle.cell_codes:
                 print('Invalid entry:', solution[row_i][col_i])
                 return False
+            
+            # store locations of 1s for checking continuous path later
             if solution[row_i][col_i] == 1:
                 filled_cell_locations.append((row_i, col_i))
     
--- tests/test_pytapa_env.py
+++ tests/test_pytapa_env.py
@@ -12,10 +12,22 @@ class TestPytapaEnv(unittest.TestCase):
             p = Puzzle(puzzle['solution'])
             self.assertEqual(p.current_state, puzzle['start'])
 
+    def test_find_all(self):
+        l = [0, 1, 3, 5, 1, 1, 5, 3, 5, 1, 1, 1]
+        subs = [[1,1], [0,1], [3,5], [3]]
+        expecteds = [[4, 9, 10], [0], [2, 7], [2, 7]]
+
+        for i in range(len(subs)):
+            actual = solver.find_all(l, subs[i])
+            expected = expecteds[i]
+            self.assertEqual(actual, expected)
+
+
     def test_validation_of_valid_tests(self):
         for puzzle in sample_puzzles.valid_sample_puzzles:
-            self.assertTrue(solver.validate_solution(puzzle['solution']))
+            print('testing with puzzle', puzzle)
+            assert solver.validate_solution(puzzle['solution'])
 
     def test_validation_of_invalid_tests(self):
         for puzzle in sample_puzzles.invalid_sample_puzzles:
-            self.assertFalse(solver.validate_solution(puzzle['solution']))
+            assert not solver.validate_solution(puzzle['solution'])
